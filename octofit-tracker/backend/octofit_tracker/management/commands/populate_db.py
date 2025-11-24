1a4d2ef1f6fca4c1bfa60d069cb1b8b890c13147
gaurav-6052 <gaurav.6052@gmail.com>
2025-11-24 07:00:25 +0000

    adding my second commit

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,59 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from djongo import models
+from pymongo import MongoClient
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        client = MongoClient(host='localhost', port=27017)
+        db = client['octofit_db']
+
+        # Drop collections if they exist
+        for col in ['users', 'teams', 'activities', 'leaderboard', 'workouts']:
+            db[col].drop()
+
+        # Create unique index on email for users
+        db.users.create_index([('email', 1)], unique=True)
+
+        # Sample users (superheroes)
+        users = [
+            {'name': 'Iron Man', 'email': 'ironman@marvel.com', 'team': 'marvel'},
+            {'name': 'Captain America', 'email': 'cap@marvel.com', 'team': 'marvel'},
+            {'name': 'Spider-Man', 'email': 'spiderman@marvel.com', 'team': 'marvel'},
+            {'name': 'Batman', 'email': 'batman@dc.com', 'team': 'dc'},
+            {'name': 'Superman', 'email': 'superman@dc.com', 'team': 'dc'},
+            {'name': 'Wonder Woman', 'email': 'wonderwoman@dc.com', 'team': 'dc'},
+        ]
+        db.users.insert_many(users)
+
+        # Teams
+        teams = [
+            {'name': 'marvel', 'members': ['ironman@marvel.com', 'cap@marvel.com', 'spiderman@marvel.com']},
+            {'name': 'dc', 'members': ['batman@dc.com', 'superman@dc.com', 'wonderwoman@dc.com']},
+        ]
+        db.teams.insert_many(teams)
+
+        # Activities
+        activities = [
+            {'user_email': 'ironman@marvel.com', 'activity': 'Running', 'duration': 30},
+            {'user_email': 'batman@dc.com', 'activity': 'Cycling', 'duration': 45},
+        ]
+        db.activities.insert_many(activities)
+
+        # Leaderboard
+        leaderboard = [
+            {'team': 'marvel', 'points': 120},
+            {'team': 'dc', 'points': 110},
+        ]
+        db.leaderboard.insert_many(leaderboard)
+
+        # Workouts
+        workouts = [
+            {'user_email': 'superman@dc.com', 'workout': 'Strength', 'suggestion': 'Lift weights'},
+            {'user_email': 'spiderman@marvel.com', 'workout': 'Cardio', 'suggestion': 'Run sprints'},
+        ]
+        db.workouts.insert_many(workouts)
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data.'))
